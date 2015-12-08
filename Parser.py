d6666cb9f14530524a58abe36b7ef90593c7dcfc
david siah <dsiah@villanova.edu>
2015-12-08 13:56:29 -0500

    parser began

--- Parser.py
+++ Parser.py
@@ -1,27 +1,21 @@
 import sys
 # (TODO) Create class for parser (instantiated with Forest Object)
 
-def R(name1, name2):
+class Parser(object):
+    def __init__(self, forest):
+        self.forest = forest
+
+    def r(self, name1, name2):
     # Finds the closest relation of 2 people
-    print("x")
-
-def X(name1, r, name2):
-    # Finds if 2 people are related with r relation
-    print('x')
-
-def W(rel, name1):
-    # Prints a list of all people relation (rel) related to name1
-    print('x')
-
-"""
-for line in sys.stdin:
-    newline = line.split()
-    if newline[0]   == 'E':
-        print('E')
-    elif newline[0] == 'R':
         print('R')
-    elif newline[0] == 'X':
+
+    def x(self, name1, r, name2):
+    # Finds if 2 people are related with r relation
         print('X')
-    elif newline[0] == 'W':
+
+    def w(self, rel, name1):
+    # Prints a list of all people relation (rel) related to name1
         print('W')
-"""
+
+    def e(self, name1, name2, child=None):
+        print('E', name1, name2, child)
--- SiahOhara.py
+++ SiahOhara.py
@@ -1,4 +1,20 @@
 import Forest, Parser, Person
+import sys
 if __name__ == '__main__':
-	print("Success!!!")
-	print("hello")
+
+  f = Forest.Forest()
+  p = Parser.Parser(f)
+
+  for line in sys.stdin:
+    newline = line.split()
+    
+    if newline[0]   == 'E' and len(newline) >= 4:
+        p.e(newline[1], newline[2], newline[3])
+    elif newline[0]   == 'E' and len(newline) < 4:
+        p.e(newline[1], newline[2])
+    elif newline[0] == 'R':
+        print('R')
+    elif newline[0] == 'X':
+        print('X')
+    elif newline[0] == 'W':
+        print('W')
