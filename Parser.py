369440c029312e3b9b63bf7625222d5f3eb31fa2
david siah <dsiah@villanova.edu>
2015-12-09 12:39:18 -0500

    parser is good until cousins case

--- Parser.py
+++ Parser.py
@@ -1,13 +1,10 @@
 import sys
 import Forest
 import Person
-# (TODO) Create class for parser (instantiated with Forest Object)
-
 
 def listoverlap(a, b):   # function to find if two lists share any elements
     s = set(b)
     return any(n in s for n in a)
-
 
 class Parser(object):
     def __init__(self, forest):
@@ -15,18 +12,17 @@ class Parser(object):
 
     def r(self, name1, name2):
     # Finds the closest relation of 2 people
-        if name1 in self.w('spouse',name2):
+        if name1 in self.w('spouse', name2):
             return 'Spouse'
-        elif name1 in self.w('parent',name2) or name2 in self.w('parent',name1):
+        elif name1 in self.w('parent', name2) or name2 in self.w('parent', name1):
             return 'Parent'
-        elif name1 in self.w('sibling',name2):
-            # only works if list of parents is in same order
+        elif name1 in self.w('sibling', name2):
             return 'Sibling'
-        elif name1 in self.w('ancestor',name2) or name2 in self.w('ancestor',name1):
+        elif name1 in self.w('ancestor', name2) or name2 in self.w('ancestor', name1):
             return 'Ancestor'
-        elif name1 in self.w('relative',name2) or name2 in self.w('relative',name1):
+        elif name1 in self.w('relative', name2) or name2 in self.w('relative', name1):
             return 'Relative'
-        elif name1 in self.w('unrelated',name2):
+        elif name1 in self.w('unrelated', name2):
             return 'Unrelated'
         else:
             return 'Cousins'
@@ -43,62 +39,73 @@ class Parser(object):
     # Prints a list of all people relation (rel) related to name1
         if rel == 'spouse':
             return self.forest.members[name1].spouse
+
         elif rel == 'parent':
             return self.forest.members[name1].parents
+
         elif rel == 'sibling':
             if self.forest.members[name1].parents[0] is not None:
-                p1=self.forest.members[self.forest.members[name1].parents[0]]
-                p2=self.forest.members[self.forest.members[name1].parents[1]]
-                c1=p1.children
-                c2=p2.children
+                p1 = self.forest.members[self.forest.members[name1].parents[0]]
+                p2 = self.forest.members[self.forest.members[name1].parents[1]]
+                c1 = p1.children
+                c2 = p2.children
                 return list(set(c1) & set(c2))
                 # from what I found this is the best way to find the intersection
                 # of two lists, but there is probably other ways
         elif rel == 'ancestor':
-            ancestors=self.forest.members[name1].parents
+            ancestors = self.forest.members[name1].parents
             for p in ancestors:    # this should work to find the list of ancestors but I haven't been able to test it
                 if self.forest.members[p].parents[0] is not None:
                     ancestors.extend(self.forest.members[p].parents)
             return ancestors
+
         elif rel == 'relative':
-            ancestors=self.w('ancestor',name1)
+            ancestors = self.w('ancestor', name1)
             relatives = []
+
             for a in ancestors:
-                relatives.extend(self.w('siblings',a))
+                relatives.extend(self.w('siblings', a))
+                
             return relatives
-        elif rel == 'cousins':
+
+        elif rel == 'cousins': # (TODO) Implement
             print('cousins')
+
         elif rel == 'unrelated': # probably needs improvement
-            ancestors = self.w('ancestor',name1)
-            everyone = self.forest.debug()
+            ancestors = self.w('ancestor', name1)
+            everyone  = self.forest.debug()
             unrelated = []
+
             for p in everyone:
-                pa = self.w('ancestors',p)
-                if not listoverlap(ancestors,pa):
+                pa = self.w('ancestors', p)
+                if not listoverlap(ancestors, pa):
                     unrelated.append(p)
             return unrelated
 
     def e(self, name1, name2, child=None):
         print('E', name1, name2, child)
 
-        if name1 not in self.forest.members: # creates parent 1 if they do not exist yet
-            p=Person.Person(name1)
+        # creates parent 1 if they do not exist yet
+        if name1 not in self.forest.members: 
+            p = Person.Person(name1)
             self.forest.add(p)
 
-        if name2 not in self.forest.members: # creates parent 2 if they do not exist
-            p=Person.Person(name2)
+        # creates parent 2 if they do not exist
+        if name2 not in self.forest.members: 
+            p = Person.Person(name2)
             self.forest.add(p)
 
         self.forest.members[name1].setSpouse(name2)
         self.forest.members[name2].setSpouse(name1)
 
-        if child!=None:
-            p=Person.Person(child,name1,name2)
+        if child != None:
+            p = Person.Person(child,name1,name2)
             self.forest.add(p)
             self.forest.members[name1].setChildren(name1)
             self.forest.members[name2].setChildren(name2)
 
         self.forest.debug()
+
 f = Forest.Forest()
 p = Parser(f)
 
